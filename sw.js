5b7356f68e6832bd15e8fa44dab5f3893b69400c
misfortunemate-png <misfortunemate@gmail.com>
2026-04-25 15:32:55 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'pv-v2.1';
+const CACHE = 'pv-v2.2';
 const ASSETS = ['./', './index.html', './manifest.json', './icon.svg',
   './js/app.js', './js/db.js', './js/utils.js', './js/state.js',
   './js/prompts.js', './js/viewer.js', './js/catalog.js', './js/settings.js', './js/dict.js'];
